e71c23c8f6a6f87d502ccdbd492409fc060c64d6
Gavle <gavleissweden@gmail.com>
2017-01-02 14:13:54 -0500

    Initial GERTi work
    Getting the startup procedure online

--- GERTi/GERTi Client.lua
+++ GERTi/GERTi Client.lua
@@ -1,4 +1,39 @@
 -- Under Construction
 local component = require("component")
 local event = require("event")
+local serialize = require("serialization")
 local m = component.modem
+local neighbors = {}
+local neighborDex = 1
+
+local function storeNeighbors(eventName, receivingModem, sendingModem, port, distance, package)
+    neighbors[neighborDex]["address"] = sendingModem
+    neighbors[neighborDex]["tier"] = tonumber(package)
+    neighborDex = neighborDex + 1
+end
+-- startup procedure
+m.open(4378)
+if modem.isWireless() then
+    modem.setStrength(500)
+end
+m.broadcast(4378, "GERTiStart")
+while true do
+    storeNeighbors(event.pull(1, "modem_message"))
+end
+
+local function sendPacket(_, finalAddress, packet)
+    --do some stuff here to send the packet on to final address
+end
+
+local function processPacket(_, packet)
+    packet = serialize.unserialize(packet)
+    --local destination
+    -- do some stuff here to process the packet and prepare for sendout
+end
+
+local function receivePacket(_, packet)
+    -- do some stuff here to receive the packet and get it ready for processing
+end
+
+event.listen("modem_message", receivePacket)
+event.listen("GERTiTransmit", processPacket)
