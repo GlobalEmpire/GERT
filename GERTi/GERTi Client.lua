d6f92a6d7e38b82c00ca5f0891feae38b55aa921
Gavle <gavleissweden@gmail.com>
2017-01-04 19:17:23 -0500

    Mostly finish startup procedure
    The GERTi startup procedure is nearly finished, although largely
    untested. This is being commited to be a guide to how the final version
    will work.

--- GERTi/GERTi Client.lua
+++ GERTi/GERTi Client.lua
@@ -2,23 +2,64 @@
 local component = require("component")
 local event = require("event")
 local serialize = require("serialization")
-local m = component.modem
+local modem = component.modem
+local tunnel = component.tunnel
+
+local tier = 100
 local neighbors = {}
 local neighborDex = 1
 
+local function handleEvent(...)
+    if eventName ~= nil then
+        storeNeighbors(...)
+    else
+        return "nillerino"
+    end
+end
 local function storeNeighbors(eventName, receivingModem, sendingModem, port, distance, package)
+    -- register neighbors for communication to gateway
     neighbors[neighborDex]["address"] = sendingModem
     neighbors[neighborDex]["tier"] = tonumber(package)
+    neighbors[neighborDex]["port"] = tonumber(port)
+    if tonumber(package) < tier then
+        tier = tonumber(package) + 1
+    end
     neighborDex = neighborDex + 1
 end
+local function sortTable(elementOne, elementTwo)
+    if tonumber(elementOne["tier"]) < tonumber(elementTwo["tier"])
+        return true
+    else
+        return false
+    end
+end
 -- startup procedure
-m.open(4378)
+-- check if linked card is available and then attempt to s
+if component.isAvailable("tunnel") then
+    tunnel.send("GERTiStart")
+    handleEvent(event.pull(1, "modem_message"))
+end
+-- now attempt to use a network card for finding neighbors
+-- open port and prepare for transmission of message
+modem.open(4378)
 if modem.isWireless() then
     modem.setStrength(500)
 end
-m.broadcast(4378, "GERTiStart")
+-- transmit broadcast to check for neighboring GERTi enabled computers
+modem.broadcast(4378, "GERTiStart")
 while true do
-    storeNeighbors(event.pull(1, "modem_message"))
+    local isNil = "not nil"
+    isNil = handleEvent(event.pull(1, "modem_message"))
+    if isNil == "nillerino" then
+        print(isNil)
+        break
+    end
+end
+
+-- sort table so that the lowest tier connections come first
+table.sort(neighbors, sortTable)
+local function transmitInformation(sendTo)
+    modem.send(sendTo, 4378, tier)
 end
 
 local function sendPacket(_, finalAddress, packet)
@@ -31,9 +72,9 @@ local function processPacket(_, packet)
     -- do some stuff here to process the packet and prepare for sendout
 end
 
-local function receivePacket(_, packet)
-    -- do some stuff here to receive the packet and get it ready for processing
+local function receivePacket(eventName, receivingModem, sendingModem, port, distance, ...)
+    -- filter out neighbor requests, and send on packet for further processing
+    
 end
 
 event.listen("modem_message", receivePacket)
-event.listen("GERTiTransmit", processPacket)
