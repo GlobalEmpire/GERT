05d179d376b1fd15bce64f1dcedaf13d0fc1301c
TYKUHN2 <tyler.h.kuhn@gmail.com>
2017-01-26 23:35:55 -0500

    Optimize
    Optimized the system to better run the code, including not blocking and
    reducing packets. Need's more but I'm off for the night here soon.

--- GERTe/GENS Server.lua
+++ GERTe/GENS Server.lua
@@ -2,8 +2,11 @@
 
 --For quick reference for communication purposes:
 --Follows similar standard to HTTP
---GENS %vers% \r\n
---%dest% %origin% \r\n\r\n
+--GENS %vers%\r\n
+--%dest% %origin%\r\n\r\n
+
+--GENS %vers%\r\n
+--FOUND: %ip%\r\n OR ERR: %err%\r\n
 
 logFile = io.open("log.txt", "a")
 logFile:write("[" + os.time() + "] -----     Initializing      -----\n")
@@ -23,6 +26,7 @@ compatible = { --Hardcoded for... reasons
 }
 
 serv = socket.tcp():listen(10)
+serv:settimeout(0) --Allows non-blocking calls
 log("Server Open")
 
 file = io.open("database.txt", "r")
@@ -32,12 +36,38 @@ function lookup(id)
 end
 
 function send(data, sock)
-	sock:send("GENS " + vers + "\r\n")
+	local str = "GENS " + vers + "\r\n"
 	for k, line in pairs(data) do
-		sock:send(line + "\r\n")
+		str = str + line + "\r\n"
 	end
-	sock:send("\r\n")
+	sock:send(str + "\r\n") --Optimized, works around Nagel's without disabling it
 	sock:close()
+end
+
+function main()
+	local client = serv:accept() --WARNING: Will block, this code won't close cleanly
+	if not client then return end --If client isn't waiting execute other operations
+	local header = client:receive() --TODO: Implement something like socket.select or something
+	if header:sub(1, 4) ~= "GENS" then
+		client:send("HTTP/1.1 400 Bad Request\r\n") --Why not? It's universal
+		send({"ERR: NOT GENS"}, client)
+		log("[W] Non-GENS from " + client:getpeername())
+		return
+	elseif not compatible[header:sub(5)] then
+		send({"ERR: NOT COMPATIBLE"}, client)
+		log("[W] Non-compatible GENS from " + client:getpeername())
+		return
+	end
+	local request = client:receive()
+	local reqID, origin, tracker = string.match(request, "(" + fullPat + ") (" + fullPat + ")"), origin + " (" + client:getpeername() +")"
+	local map = lookup(reqID)
+	if not map then
+		send({"ERR: NOT FOUND"}, client)
+		log("[E] " + tracker + " attempted to find " + reqID + " but failed")
+		return
+	end
+	send({"FOUND: " + map}, client)
+	log("[I] " + tracker + " found " reqID + " (" + map ")")
 end
 
 log("Building Database")
@@ -51,25 +81,5 @@ end
 log("-----Initialization Finished-----")
 
 while true do --Primary execution loop
-	local client = serv:accept() --WARNING: Will block, this code won't close cleanly
-	local header = client:receive()
-	if header:sub(1, 4) ~= "GENS" then
-		client:send("HTTP/1.1 400 Bad Request\r\n") --Why not? It's universal
-		send({"ERR: NOT GENS"}, client)
-		log("[W] Non-GENS from " + client:getpeername())
-	elseif not compatible[header:sub(5)] then
-		send({"ERR: NOT COMPATIBLE"}, client)
-		log("[W] Non-compatible GENS from " + client:getpeername())
-	else
-		local request = client:receive()
-		local reqID, origin, tracker = string.match(request, "(" + fullPat + ") (" + fullPat + ")"), origin + " (" + client:getpeername() +")"
-		local map = lookup(reqID)
-		if not map then
-			send({"ERR: NOT FOUND"}, client)
-			log("[E] " + tracker + " attempted to find " + reqID + " but failed")
-		else
-			send({"FOUND: " + map}, client)
-			log("[I] " + tracker + " found " reqID + " (" + map ")")
-		end
-	end
+	main()
 end
