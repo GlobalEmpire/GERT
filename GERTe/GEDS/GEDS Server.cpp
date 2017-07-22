26c32f11c828bc4660f8d8d9c86723c02e67c3d3
TYKUHN2 <tyler.h.kuhn@gmail.com>
2017-07-22 15:07:38 -0400

    Documentation Update
    Fixed and added some documentation

--- GERTe/GEDS/GEDS Server.cpp
+++ GERTe/GEDS/GEDS Server.cpp
@@ -11,76 +11,78 @@
 	https://github.com/GlobalEmpire/GERT/blob/master/License.md
  */
 
+#define VERSION "1.0.0" //Creates VERSION shortcut so we can update this instead for easy access.
+
 typedef unsigned char UCHAR; //Creates UCHAR shortcut for Unsigned Character
-typedef unsigned short ushort;
+typedef unsigned short ushort; //Created ushort shortcut for Unsigned Short
 
-#include <thread> //Include thread type
-#include <signal.h> //Include signal processing API
-#include <iostream>
-#include "netty.h"
-#include "libLoad.h"
-#include "overwatch.h"
-#include "fileMngr.h"
-#include <exception>
-using namespace std; //Default namespace to std
+#include <thread> //Include thread type for server threading
+#include <signal.h> //Include signal processing API for error catching
+#include <iostream> //Include IO streams, I don't remember why
+#include "netty.h" //Include netcode header for entering server process
+#include "libLoad.h" //Include library loading header for loading all protocol libraries
+#include "overwatch.h" //Include overwatch header for error checking and "recovery"
+#include "fileMngr.h" //Include file manager library for loading and saving databases
+#include <exception> //Load exception library for terminate() hook
+using namespace std; //Default namespace to std so I don't have to type out std::cout or any other crap
 
-enum status {
-	NORMAL,
-	NO_LIBS,
-	LIB_LOAD_ERR,
-	PEER_LOAD_ERR,
-	KEY_LOAD_ERR,
-	UNKNOWN_CRITICAL,
-	UNKNOWN_MINOR
+enum status { //Create a list of error codes
+	NORMAL, //YAY WE RAN AS WE SHOULD HAVE (0)
+	NO_LIBS, //Uhm, this server isn't all inclusive (1)
+	LIB_LOAD_ERR, //Unknown error when loading protocol libraries (2)
+	PEER_LOAD_ERR, //Unknown error when loading peer database (3)
+	KEY_LOAD_ERR, //Unknown error when loading key database (4)
+	UNKNOWN_CRITICAL, //Unknown error which resulted in a crash, either terminate() or SIGSEGV (5)
+	UNKNOWN_MINOR //Unknown error caused closing, not necessarily a crash (6)
 };
 
 volatile bool running = false; //SIGINT tracker
-bool debugMode = false;
+bool debugMode = false; //Set debug mode to false by default
 
-char * LOCAL_IP = nullptr;
-char * peerPort = "59474";
-char * gatewayPort = "43780";
+char * LOCAL_IP = nullptr; //Set local address to predictable null value for testing
+char * peerPort = "59474"; //Set default peer port
+char * gatewayPort = "43780"; //Set default gateway port
 
 void shutdownProceedure(int param) { //SIGINT handler function
-	if (running) {
-		warn("User requested shutdown. Flipping the switch!");
+	if (running) { //If we actually started running
+		warn("User requested shutdown. Flipping the switch!"); //Notify the user because reasons
 		running = false; //Flip tracker to disable threads and trigger main thread's cleanup
-	} else {
-		error("Server wasn't in running state when SIGINT was raised.");
-		error("!!! Forcing termination of server. !!!");
-		exit(UNKNOWN_MINOR);
+	} else { //We weren't actually running?
+		error("Server wasn't in running state when SIGINT was raised."); //Warn user of potential error
+		error("!!! Forcing termination of server. !!!"); //Warn user we are stopping
+		exit(UNKNOWN_MINOR); //Exit with correct exit code
 	}
 };
 
-void OHCRAPOHCRAP(int param) {
-	int errCode = emergencyScan();
-	saveResolutions();
-	if (errCode < 2) {
-		savePeers();
-		debug("Peers uncorrupted, peer file updated.");
+void OHCRAPOHCRAP(int param) { //Uhm, we've caused a CPU error
+	int errCode = emergencyScan(); //Scan structure database for faults, there will be faults
+	saveResolutions(); //Save key resolutions, these don't easily if ever corrupt
+	if (errCode < 2) { //If no peer error is detected (Wait what faulted?)
+		savePeers(); //Save the peers database
+		debug("Peers uncorrupted, peer file updated."); //Report to user using debug facility
 	}
-	cout << "Well, this is the end\n";
-	cout << "I've read too much, and written so far\n";
+	cout << "Well, this is the end\n"; //Print a little poem to the user
+	cout << "I've read too much, and written so far\n"; //Reference to memory error
 	cout << "But here I am, faulting to death\n";
-	cout << "\nSIGSEGV THROWN: SEGMENTATION FAULT.\n";
-	cout << "SIGSEGV is a critical error thrown by the operating system\n";
-	cout << "It signifies a MASSIVE error that won't correct itself and probably can't be fixed without changing the code.\n";
-	cout << "Please post a bug report including any potentially useful logs.\n";
-	exit(UNKNOWN_CRITICAL);
+	cout << "\nSIGSEGV THROWN: SEGMENTATION FAULT.\n"; //Actual error
+	cout << "SIGSEGV is a critical error thrown by the operating system\n"; //Information for uninformed user
+	cout << "It signifies a MASSIVE error that won't correct itself and probably can't be fixed without changing the code.\n"; //Create sense of urgency in user
+	cout << "Please post a bug report including any potentially useful logs.\n"; //Call to action trying to get user to submit report
+	exit(UNKNOWN_CRITICAL); //Exit with correct exit code
 }
 
-void errHandler() {
-	int errCode = emergencyScan();
-	saveResolutions();
-	if (errCode < 2) {
-		savePeers();
-		debug("Peers uncorrupted, peer file updated.");
+void errHandler() { //Error catcher, provides minor error recovery facilities
+	int errCode = emergencyScan(); //Scan structure databases for faults
+	saveResolutions(); //Save key resolutions, these don't easily if ever corrupt
+	if (errCode < 2) { //If no peer error is detected
+		savePeers(); //Save the peers database
+		debug("Peers uncorrupted, peer file updated."); //Report to user using debug facility
 	}
-	cout << "Unknown error, system called terminate() with code " << to_string(errno) << "\n";
-	exit(UNKNOWN_CRITICAL);
+	cout << "Unknown error, system called terminate() with code " << to_string(errno) << "\n"; //Report fault to user
+	exit(UNKNOWN_CRITICAL); //Exit with correct exit code
 }
 
-void printHelp() {
+void printHelp() { //Prints help on parameters
 	cout << "Requires atleast the -a parameter\n";
 	cout << "-a publicIP   Specifies the public IP to prevent loops.\n";
 	cout << "-p port       Specifies the port for GEDS servers to connect to (default 59474)\n";
@@ -90,108 +92,111 @@ void printHelp() {
 	exit(0);
 }
 
-void processArgs(int argc, char* argv[]) {
-	if (argc < 2) {
-		printHelp();
+void processArgs(int argc, char* argv[]) { //Process and interpret command line arguments
+	if (argc < 2) { //We require a certain amount of arguments
+		printHelp(); //Print help if they arn't given
 	}
-	for (int i = 1; i < argc; i++) {
-		string curArg = argv[i];
-		if (curArg == "-d") {
+	for (int i = 1; i < argc; i++) { //Loop through the arguments
+		string curArg = argv[i]; //Convert it to a string for easier access
+		if (curArg == "-d") { //Process the -d Debug messages flag
 			debugMode = true;
-		} else if (curArg == "-a") {
-			if (argc == i)
-				printHelp();
-			LOCAL_IP = argv[++i];
-		} else if (curArg  == "-p") {
-			if (argc == i)
-				printHelp();
-			peerPort = argv[++i];
-		} else if (curArg == "-g") {
-			if (argc == i)
-				printHelp();
-			gatewayPort = argv[++i];
-		} else if (curArg == "-h")
-				printHelp();
+		} else if (curArg == "-a") { //Process the local address flag (REQUIRED)
+			if (argc == i) //Address flag is missing the actual address
+				printHelp(); //Print help, user might not know how to use the flag
+			LOCAL_IP = argv[++i]; //Local address set to next argument. Also skips the next argument
+		} else if (curArg  == "-p") { //Process the local GEDS port flag
+			if (argc == i) //If the port flag is missing the actual port
+				printHelp(); //Print help, user might not know how to use the flag
+			peerPort = argv[++i]; //Local GEDS port is set to the next argument. Also skips the next argument
+		} else if (curArg == "-g") { //Process the gateway port flag
+			if (argc == i) //If the port flag is missing the actual port
+				printHelp(); //Print help, user might not know how to use the flag
+			gatewayPort = argv[++i]; //Local GEDS port is set to the next argument. Also skips the next argument
+		} else if (curArg == "-h") //Process the help flag
+				printHelp(); //Prints help, user requested it
 	}
-	if (LOCAL_IP == nullptr)
-		printHelp();
+	if (LOCAL_IP == nullptr) //If the local address was not set
+		printHelp(); //User forgot to include local address, print help in case they don't know they have to
 }
 
 int main( int argc, char* argv[] ) {
 
-	processArgs(argc, argv);
-	debug((string)"Processed arguments. Gateway port: " + gatewayPort + " Peer port: " + peerPort + " Local IP: " + LOCAL_IP + " Debug mode: " + to_string(debugMode) + " (should be 1)");
+	cout << "GEDS Server v" << VERSION << endl; //Print version information
+	cout << "Copyright 2017" << endl; //Print simple copyright information
 
-	startLog();
+	processArgs(argc, argv); //Process command line argument
+	debug((string)"Processed arguments. Gateway port: " + gatewayPort + " Peer port: " + peerPort + " Local IP: " + LOCAL_IP + " Debug mode: " + to_string(debugMode) + " (should be 1)"); //Print to debug processed arguments
 
-	set_terminate(errHandler);
-	signal(SIGSEGV, &OHCRAPOHCRAP);
+	startLog(); //Create log handles
+
+	set_terminate(errHandler); //Causes terminate() to trigger our code instead
+	signal(SIGSEGV, &OHCRAPOHCRAP); //Catches the SIGSEGV CPU fault
 	signal(SIGINT, &shutdownProceedure); //Hook SIGINT with custom handler
 
-	debug("Loading libraries");
-	int libErr = loadLibs(); //Load gelib files
+	debug("Loading libraries"); //Use debug to notify user where we are in the loading process
+	int libErr = loadLibs(); //Load protocol library files
 
 	switch (libErr) { //Test for errors loading libraries
-		case EMPTY:
-			error("No libraries detected.");
-			return NO_LIBS;
-		case UNKNOWN:
-			error("Unknown error occurred while loading libraries.");
-			return LIB_LOAD_ERR;
+		case EMPTY: //Test if there are no libraries
+			error("No libraries detected."); //Report error to user
+			return NO_LIBS; //Exit with correct exit code
+		case UNKNOWN: //Test for unknown library loading error
+			error("Unknown error occurred while loading libraries."); //Report error to user
+			return LIB_LOAD_ERR; //Exit with correct exit code
 	}
 
-	debug("Loading peers");
-	int result = loadPeers();
+	debug("Loading peers"); //Use debug to notify user where we are in the loading process
+	int result = loadPeers(); //Load the peer database
 
-	if (result != NORMAL) {
-		error("Failed to load peers");
-		return PEER_LOAD_ERR;
+	if (result != NORMAL) { //Check if peers failed to load
+		error("Failed to load peers"); //Report error to user
+		return PEER_LOAD_ERR; //Exit with correct exit code
 	}
 
-	debug("Loading resolutions");
-	int result2 = loadResolutions();
+	debug("Loading resolutions"); //Use debug to notify user where we are in the loading process
+	int result2 = loadResolutions(); //Load the key resolution database
 
-	if (result2 != NORMAL) {
-		error("Failed to load keys");
-		return KEY_LOAD_ERR;
+	if (result2 != NORMAL) { //Check if keys failed to load
+		error("Failed to load keys"); //Report error to user
+		return KEY_LOAD_ERR; //Exit with correct exit code
 	}
 
-	debug("Starting servers");
-	startup(); //Startup servers
+	debug("Starting servers"); //Use debug to notify user where we are in the loading process
+	startup(); //Startup server sockets
 
-	debug("Building peer web");
-	buildWeb();
+	debug("Building peer web"); //Use debug to notify user where we are in the loading process
+	buildWeb(); //Connect to online peers and update connection database
 
-	running = true;
+	running = true; //We've officially started running! SIGINT is now not evil!
 
-	debug("Starting overwatch garbage sorter");
-	thread watcher(overwatch);
+	debug("Starting overwatch garbage sorter"); //Use debug to notify user where we are in the loading process
+	thread watcher(overwatch); //Create overwatch thread, proactively removes bad links and closed sockets (socket errors)
 
-	debug("Starting message processor");
-	thread processor(process);
+	debug("Starting message processor"); //Use debug to notify user where we are in the loading process
+	thread processor(process); //Create message processor thread
 
-	debug("Starting main server loop");
-	runServer();
-	warn("Primary server killed.");
+	debug("Starting main server loop"); //Use debug to notify user where we are in the loading process
+	runServer(); //Process incoming connections (not messages)
+	warn("Primary server killed."); //Notify user we've stopped accepting incoming connections
 
 	//Shutdown and Cleanup sequence
-	debug("Waiting for message processor to exit");
+	debug("Waiting for message processor to exit"); //Notify user where we are in the shutdown process
 	processor.join(); //Cleanup processor (wait for it to die)
-	warn("Processor killed, program ending.");
+	warn("Processor killed, program ending."); //Notify the user we've stopped processing messages
 
-	debug("Waiting for overwatch garbage sorter to exit");
-	watcher.join();
+	debug("Waiting for overwatch garbage sorter to exit"); //Notify user where we are in the shutdown process
+	watcher.join(); //Cleanup overwatch (wait for it to die)
 
-	debug("Cleaning up servers");
-	shutdown();//Cleanup servers
+	debug("Cleaning up servers"); //Notify user where we are in the shutdown process
+	shutdown(); //Cleanup servers
 	
-	debug("Saving peers");
-	savePeers();
+	debug("Saving peers"); //Notify user where we are in the shutdown process
+	savePeers(); //Save the peers database to file
 
-	debug("Saving resolutions");
-	saveResolutions();
+	debug("Saving resolutions"); //Notify user where we are in the shutdown process
+	saveResolutions(); //Save the keys database to file
 
-	stopLog();
+	stopLog(); //Release log handles
 
-	return NORMAL; //Return with exit state "NORMAL" (0)
+	return NORMAL; //Exit with correct exit code
 }
--- GERTe/GEDS/fileMngr.cpp
+++ GERTe/GEDS/fileMngr.cpp
@@ -1,87 +1,86 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS //Windows grumble grumble
 
-#include "peerManager.h"
-#include "keyMngr.h"
+#include "peerManager.h" //Include peer manager so that we can write to the peer database
+#include "keyMngr.h" //Include key manager so we can write to the key database
 
-typedef map<GERTaddr, GERTkey>::iterator keyIter;
+typedef map<GERTaddr, GERTkey>::iterator keyIter; //Define what a iterator for keys is
 
-enum errors {
-	OK,
-	NOK
+enum errors { //Define a list of error codes
+	OK, //No error has occurred
+	NOK //Error has occurred
 };
 
-extern char * LOCAL_IP;
+extern char * LOCAL_IP; //Grab the local address
 
-extern map<ipAddr, knownPeer> peerList;
-extern map<GERTaddr, GERTkey> resolutions;
+extern map<GERTaddr, GERTkey> resolutions; //Grab the key database
 
-int loadPeers() {
-	FILE* peerFile = fopen("peers.geds", "rb");
-	if (peerFile == nullptr)
-		return NOK;
+int loadPeers() { //Load peers from a file
+	FILE* peerFile = fopen("peers.geds", "rb"); //Open the file in binary mode
+	if (peerFile == nullptr) //If the file failed to open
+		return NOK; //Return with an error
 	while (true) {
-		unsigned long ip;
-		fread(&ip, 4, 1, peerFile); //Why must I choose between 1, 4 and 4, 1? Or 2, 2?
-		if (feof(peerFile) != 0)
-			break;
-		USHORT rawPorts[2];
-		fread(&rawPorts, 2, 2, peerFile);
-		rawPorts[0] = ntohs(rawPorts[0]);
+		unsigned long ip; //Define a storage variable for an address
+		fread(&ip, 4, 1, peerFile); //Why must I choose between 1, 4 and 4, 1? Or 2, 2? Store an IP into previous variable
+		if (feof(peerFile) != 0) //If the file has nothing left
+			break; //We're done
+		USHORT rawPorts[2]; //Make two storage variables for ports
+		fread(&rawPorts, 2, 2, peerFile); //Fill those variables
+		rawPorts[0] = ntohs(rawPorts[0]); //Fix those variables for platform compatibility
 		rawPorts[1] = ntohs(rawPorts[1]);
-		portComplex ports = {rawPorts[0], rawPorts[1]};
-		ipAddr ipClass = ip;
-		if (ipClass.stringify() != LOCAL_IP)
-			log("Importing peer " + ipClass.stringify() + ":" + ports.stringify());
-		addPeer(ipClass, ports);
+		portComplex ports = {rawPorts[0], rawPorts[1]}; //Reformat those variables into a structure
+		ipAddr ipClass = ip; //Reformat the address into a structure
+		if (ipClass.stringify() != LOCAL_IP) //If the string version of the address isn't what the local address is set to
+			log("Importing peer " + ipClass.stringify() + ":" + ports.stringify()); //Print out what we've imported
+		addPeer(ipClass, ports); //Add peer to the database
 	}
-	fclose(peerFile);
-	return OK;
+	fclose(peerFile); //Close the file
+	return OK; //Return without errors
 }
 
-int loadResolutions() {
-	FILE* resolutionFile = fopen("resolutions.geds", "rb");
-	if (resolutionFile == nullptr)
-		return NOK;
+int loadResolutions() { //Load key resolutions from a file
+	FILE* resolutionFile = fopen("resolutions.geds", "rb"); //Open the file in binary mode
+	if (resolutionFile == nullptr) //If the file failed to open
+		return NOK; //Return with an error
 	while (true) {
-		UCHAR bufE[3];
-		UCHAR bufI[3];
-		fread(&bufE, 1, 3, resolutionFile);
-		fread(&bufI, 1, 3, resolutionFile);
-		if (feof(resolutionFile) != 0)
-			break;
-		GERTaddr addr{bufE, bufI};
-		char buff[20];
-		fread(&buff, 1, 20, resolutionFile);
-		GERTkey key(buff);
-		log("Imported resolution for " + addr.stringify());
-		addResolution(addr, key);
+		UCHAR bufE[3]; //Create a storage variable for the external portion of the address
+		UCHAR bufI[3]; //Create a storage variable for the internal portion of the address
+		fread(&bufE, 1, 3, resolutionFile); //Fill the external address
+		fread(&bufI, 1, 3, resolutionFile); //Fill the internal address
+		if (feof(resolutionFile) != 0) //If the file is at the end
+			break; //We're done
+		GERTaddr addr{bufE, bufI}; //Reformat the address portions into a single structure
+		char buff[20]; //Create a storage variable for the key
+		fread(&buff, 1, 20, resolutionFile); //Fill the key
+		GERTkey key(buff); //Reformat the key
+		log("Imported resolution for " + addr.stringify()); //Print what we've imported
+		addResolution(addr, key); //Add the key to the database
 	}
-	fclose(resolutionFile);
-	return OK;
+	fclose(resolutionFile); //Close the file
+	return OK; //Return without an error
 }
 
-void savePeers() {
-	FILE * peerFile = fopen("peers.geds", "wb");
-	for (knownIter iter; !iter.isEnd(); iter++) {
-		knownPeer tosave = *iter;
-		unsigned long addr = htonl((unsigned long)tosave.addr.addr.s_addr);
-		fwrite(&addr, 4, 1, peerFile);
-		unsigned short gateport = htons(tosave.ports.gate);
-		fwrite(&gateport, 2, 1, peerFile);
-		unsigned short peerport = htons(tosave.ports.peer);
-		fwrite(&peerport, 2, 1, peerFile);
+void savePeers() { //Save the database to a file
+	FILE * peerFile = fopen("peers.geds", "wb"); //Open the file in binary mode
+	for (knownIter iter; !iter.isEnd(); iter++) { //For each peer in the database
+		knownPeer tosave = *iter; //Gets the next peer
+		unsigned long addr = htonl((unsigned long)tosave.addr.addr.s_addr); //Grabs the IP address and converts it to cross-platform mode
+		fwrite(&addr, 4, 1, peerFile); //Writes it to file
+		unsigned short gateport = htons(tosave.ports.gate); //Converts gateway port to cross-platform mode
+		fwrite(&gateport, 2, 1, peerFile); //Writes it to file
+		unsigned short peerport = htons(tosave.ports.peer); //Converts peer port to cross-platform mode
+		fwrite(&peerport, 2, 1, peerFile); //Write it to file
 	}
-	fclose(peerFile);
+	fclose(peerFile); //Close the file
 }
 
-void saveResolutions() {
-	FILE * resolutionFile = fopen("resolutions.geds", "wb");
-	for (keyIter iter = resolutions.begin(); iter != resolutions.end(); iter++) {
-		GERTaddr addr = iter->first;
-		GERTkey key = iter->second;
-		fwrite(&addr.eAddr, 1, 3, resolutionFile);
-		fwrite(&addr.iAddr, 1, 3, resolutionFile);
-		fwrite(&key.key, 1, 20, resolutionFile);
+void saveResolutions() { //Save key resolutions to file
+	FILE * resolutionFile = fopen("resolutions.geds", "wb"); //Open file in binary mode
+	for (keyIter iter = resolutions.begin(); iter != resolutions.end(); iter++) { //For each key in the database
+		GERTaddr addr = iter->first; //Get the associated address
+		GERTkey key = iter->second; //Get the key
+		fwrite(&addr.eAddr, 1, 3, resolutionFile); //Write the external address portion to file
+		fwrite(&addr.iAddr, 1, 3, resolutionFile); //Write the internal address portion to file
+		fwrite(&key.key, 1, 20, resolutionFile); //Write the key to file
 	}
-	fclose(resolutionFile);
+	fclose(resolutionFile); //Close the file
 }
