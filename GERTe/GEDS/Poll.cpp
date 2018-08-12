6c381c8c168aa5aaeb35524d0e255df940598968
TYKUHN2 <tyler.h.kuhn@gmail.com>
2018-08-12 02:04:40 -0400

    Conditionalized Current Code Moved current code into blocks only compiled if not compiled for Windows. Stacktrace won't be implemented on Windows currently due to complexity. Poll is temporarily incorrectly implemented on Windows pending compatibility rewrite. Will crash build.

--- GERTe/GEDS/Poll.cpp
+++ GERTe/GEDS/Poll.cpp
@@ -1,8 +1,11 @@
 #include "Poll.h"
-#include <sys/epoll.h>
 #include <vector>
 
-typedef vector<Event_Data*> LList;
+#ifndef _WIN32
+#include <sys/epoll.h>
+#endif
+
+typedef std::vector<Event_Data*> LList;
 
 void removeTracker(int fd, LList* tracker) {
 	for (LList::iterator iter = tracker->begin(); iter != tracker->end(); iter++)
@@ -16,12 +19,15 @@ void removeTracker(int fd, LList* tracker) {
 }
 
 Poll::Poll() {
+#ifndef _WIN32
 	efd = epoll_create(0);
 
 	tracker = new LList;
+#endif
 }
 
 Poll::~Poll() {
+#ifndef _WIN32
 	close(fd);
 
 	for (Event_Data * data : *(LList*)tracker)
@@ -35,9 +41,11 @@ Poll::~Poll() {
 	}
 
 	delete tracker;
+#endif
 }
 
 void Poll::add(SOCKET fd, void * ptr) { //Adds the file descriptor to the pollset and tracks useful information
+#ifndef _WIN32
 	epoll_event newEvent;
 
 	newEvent.events = EPOLLIN;
@@ -53,20 +61,25 @@ void Poll::add(SOCKET fd, void * ptr) { //Adds the file descriptor to the pollse
 		throw errno;
 
 	LList.push_back(data);
+#endif
 }
 
 void Poll::remove(SOCKET fd) {
+#ifndef _WIN32
 	if (epoll_ctl(efd, EPOLL_CTL_DEL, fd, nullptr) == -1)
 		throw errno;
 
 	removeTracker(fd, (LList*)tracker);
+#endif
 }
 
 Event_Data Poll::wait() { //Awaits for an event on a file descriptor. Returns the Event_Data for a single event
+#ifndef _WIN32
 	epoll_event eEvent;
 
 	if (epoll_wait(efd, &eEvent, 1, -1) == -1)
 		throw errno;
 
 	return *(eEvent.data.ptr);
+#endif
 }
