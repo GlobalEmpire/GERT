440cd85f2347742676fb29eafdf59fbfdce44204
TYKUHN2 <tyler.h.kuhn@gmail.com>
2019-02-16 01:22:34 -0500

    Fixed Iterator Bounds Issue Fixed an issue where iterators could be out of bounds

--- GERTe/GEDS/peerManager.cpp
+++ GERTe/GEDS/peerManager.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 extern map<IP, Peer*> peers;
 
-bool peerIter::isEnd() { return (ptr == peers.end()) || (ptr == ++peers.end()); }
-peerIter peerIter::operator++ (int a) { return (ptr++, *this); }
+bool peerIter::isEnd() { return (ptr == peers.end()) || (ptr == peers.end()); }
+peerIter peerIter::operator++ (int a) { ptr++; return (*this); }
 peerIter::peerIter() : ptr(peers.begin()) {};
 Peer* peerIter::operator*() { return ptr->second; }
