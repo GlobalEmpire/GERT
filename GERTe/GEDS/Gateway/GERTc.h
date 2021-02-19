984b1840f4b1071114c9603ddba2e2a7a33a3fd1
Tyler Kuhn <contact@tyknet.tk>
2021-02-18 19:26:21 -0500

    Updated Old Files
    Removed unused GERTc constructor.
    Commented out old query line for later implementation.

--- GERTe/GEDS/Gateway/GERTc.h
+++ GERTe/GEDS/Gateway/GERTc.h
@@ -6,7 +6,6 @@ public:
 	Address external;
 	Address internal;
 	GERTc(const std::string& data) : external(data.substr(0, 3)), internal(data.substr(3, 3)) {};
-	GERTc(const unsigned char* ptr) : external(ptr), internal(ptr + 3) {};
 	GERTc() : external(), internal() {};
 	std::string stringify() const { return external.stringify() + ":" + internal.stringify(); }
 	std::string tostring() const { return external.tostring() + internal.tostring(); }
--- GERTe/GEDS/Peer/query.cpp
+++ GERTe/GEDS/Peer/query.cpp
@@ -1,12 +1,9 @@
 #include "query.h"
 #include <thread>
 #include <chrono>
-#include "peerManager.h"
-#include "../Gateway/routeManager.h"
-#include "../Networking/netty.h"
 using namespace std;
 
 void query(Address target) {
-	Peer::broadcast(string({8}) + target.tostring());
+	// TODO: broadcast(string({8}) + target.tostring());
 	this_thread::sleep_for((chrono::duration<int>) 5);
 }
