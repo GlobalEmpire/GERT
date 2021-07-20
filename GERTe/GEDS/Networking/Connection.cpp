f3de54bca819165b9b8491e9d604988aadb163ed
Tyler Kuhn <contact@tyknet.tk>
2021-07-19 21:50:52 -0400

    Updated Connection
    Connection now tracks type for logging instead of some obtuse constructor trick.
    Connection now finalizes process override, sub-type use _process for more specific processing.
    Connection has new atleast function to ensure at least x bytes can be read.
    Connection overrides valid to check if the connection has been closed (calls super::valid first.)
    Updated Connection with IPv6 support.
    Added handshake function for sub-type handshaking needs.
    Simplified Connection constructor by accepting either an address for outbound or a socket for inbound.
    Updated connection to use new Socket abstraction.
    Connection write now handles connection closed, flushing the read buffer and preventing further writes, closing the socket when done.
    Connection had issues on some platforms, added a mutex for safety when entering _process.
    Connection now smartly writes and reads version numbers for handshaking.
    Connection internally reads handshake errors.
    Connection runs asynchronously (mostly.)
    Connection now manages telling the socket to connect.

--- GERTe/GEDS/Networking/Connection.cpp
+++ GERTe/GEDS/Networking/Connection.cpp
@@ -1,112 +1,207 @@
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
+#define ioctl ioctlsocket
+
 #include <WinSock2.h>
+#include <ws2ipdef.h>
 #pragma comment(lib, "Ws2_32.lib")
 #else
+#define INVALID_SOCKET -1
+
 #include <sys/socket.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+
+typedef unsigned long u_long;
 #endif
 
 #include "Connection.h"
-#include "netty.h"
 #include "../Util/logging.h"
 #include "../Util/Versioning.h"
 #include "../Util/Error.h"
+#include <stdexcept>
+#include <cstring>
 
-std::string Connection::read(int num) const {
-	char * buf = new char[num];
-	int len = recv(this->sock, buf, num, 0);
-    std::string data{ buf, (size_t)len };
-    delete[] buf;
-	return data;
+static_assert(sizeof(ipv6) == sizeof(in6_addr), "ipv6 != in6_addr length");
+
+constexpr unsigned long nonblock = 1;
+
+bool Connection::handshake() {
+    return true;
 }
 
-Connection::Connection(SOCKET socket, const std::string& type) : sock(socket) {
-#ifdef _WIN32
-#define PTR char*
-#else
-#define PTR void*
-#endif
+Connection::Connection(uint32_t addr, uint16_t port, const std::string& name) : Socket(false, true),
+                                                                                type(name), outbound(true)  {
+    sockaddr_in tgt = {
+            AF_INET,
+            htons(port)
+    };
 
+    memcpy(&tgt.sin_addr, &addr, sizeof(addr));
+
+    if (connect(sock, (sockaddr*)&tgt, sizeof(sockaddr_in)))
+        throw std::runtime_error{ socketError("Failed to connect: ") };
+
+    ioctl(sock, FIONBIO, (u_long*)&nonblock); //Ensure socket is in blocking mode
+
+    write(ThisVersion.tostring()); // We write first
+}
+
+Connection::Connection(ipv6 addr, uint16_t port, const std::string& name) : Socket(true, true),
+                                                                            type(name), outbound(true)  {
+    sockaddr_in6 tgt = {
+            AF_INET6,
+            htons(port),
+            0,
+            {},
+            0
+    };
+
+    memcpy(&tgt.sin6_addr, &addr, sizeof(addr));
+
+    if (connect(sock, (sockaddr*)&tgt, sizeof(sockaddr_in6)))
+        throw std::runtime_error{socketError("Failed to connect: ") };
+
+    ioctl(sock, FIONBIO, (u_long*)&nonblock); //Ensure socket is in blocking mode
+
+    write(ThisVersion.tostring()); // We write first
+}
+
+Connection::Connection(SOCKET socket, const std::string& type) : Socket(socket), type(type), outbound(false) {
 #ifdef WIN32
 	WSAEventSelect(socket, nullptr, 0); //Clears all events associated with the new socket
-	u_long nonblock = 0;
-	ioctlsocket(socket, FIONBIO, &nonblock); //Ensure socket is in blocking mode
 #endif
 
-#ifndef _WIN32
-	timeval opt = { 1, 0 };
-#else
-	int opt = 2000;
-#endif
-
-	setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, (PTR)&opt, sizeof(opt));
-
-#undef PTR
-
-	int result = recv(socket, (char*)vers, 2, 0);
-
-	if (result != 2) {
-		socketError("Error reading version information: ");
-		return;
-	}
-
-	log(type + " using v" + std::to_string(vers[0]) + "." + std::to_string(vers[1]));
-
-	if (vers[0] != ThisVersion.major) { //Determine if major number is not supported
-		write("\0\0\0");
-		warn(type + "'s version wasn't supported!");
-		return;
-	}
-	else if (vers[1] > ThisVersion.minor)
-		vers[1] = ThisVersion.minor;
-
-	valid = true;
+	ioctl(sock, FIONBIO, (u_long*)&nonblock); //Ensure socket is in blocking mode
 }
 
-Connection::Connection(SOCKET socket) : sock(socket) {
-#ifdef _WIN32
-#define PTR char*
-#else
-#define PTR void*
-#endif
+void Connection::process() {
+    if (vers[0] == 0 && vers[1] == 0) { // Pre-handshake
+        size_t result = ::recv(sock, (char*)vers, 2, MSG_PEEK);
 
-#ifdef WIN32
-    WSAEventSelect(socket, nullptr, 0); //Clears all events associated with the new socket
-    u_long nonblock = 0;
-    ioctlsocket(socket, FIONBIO, &nonblock); //Ensure socket is in blocking mode
-#endif
+        if (result <= 0) {
+            error(socketError("Error reading version information: "));
+            close();
+            return;
+        } else if (result == 1) // Not enough bytes yet
+            return;
 
-#ifndef _WIN32
-    timeval opt = { 1, 0 };
-#else
-    int opt = 2000;
-#endif
+        ::recv(sock, (char*)vers, 2, 0);
 
-    setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, (PTR)&opt, sizeof(opt));
+        if (vers[0] == 0 && vers[1] == 0) {
+            char errcode;
+            result = ::recv(sock, &errcode, 1, MSG_WAITALL);
+            close();
 
-#undef PTR
+            if (result < 1)
+                error(socketError("Error processing handshake error: "));
+            else
+                switch (errcode) {
+                    case 0:
+                        error(type + " reported a version error.");
+                        break;
+                    case 1:
+                        error(type + " reported a unknown address error. This is abnormal.");
+                        break;
+                    case 2:
+                        error(type + " reported an unknown error.");
+                        break;
+                    default:
+                        error(type + " reported an unrecognized error.");
+                }
 
-    write(ThisVersion.tostring());
-    valid = true;
+            return;
+        }
+
+
+        if (vers[0] != ThisVersion.major) { //Determine if major number is not supported
+            write(std::string{"\0\0\0", 3});
+            warn(type + "'s version wasn't supported!");
+            close();
+            return;
+        }
+
+        log(type + " using v" + std::to_string(vers[0]) + "." + std::to_string(vers[1]));
+
+        if (vers[1] > ThisVersion.minor)
+            vers[1] = ThisVersion.minor;
+    }
+
+    if (!ready) {
+        if (!handshake())
+            return;
+
+        ready = true;
+        if (!outbound)
+            write(ThisVersion.tostring()); // We still need to write
+    }
+
+    std::lock_guard guard{ safety };
+    while (atleast(1))
+        _process();
 }
 
-Connection::~Connection() {
-    if (valid)
+std::string Connection::read(int num) {
+    if (num == 0) {
+        warn("Connection tried to read 0, this is usually a bug.");
+        return "";
+    }
+
+    if (sock == INVALID_SOCKET)
+        return "";
+
+    char * buf = new char[num];
+    size_t len = recv(sock, buf, num, 0);
+    if (len == -1) {
+        if (wouldBlock())
+            return "";
+
         close();
+
+        throw std::runtime_error{ socketError("Failed to read from socket") };
+    } else if (len == 0) {
+        close(); // We ought to get something, so close for safety.
+        return "";
+    }
+
+    std::string data{ buf, (size_t)len };
+    delete[] buf;
+    return data;
 }
 
-void Connection::write(const std::string& data) const {
-    ::send(sock, data.c_str(), (ULONG)data.length(), 0);
+bool Connection::atleast(int len) {
+    char* dump = new char[len];
+    size_t num = recv(sock, dump, len, MSG_PEEK);
+    delete[] dump;
+
+    if (num == -1 && !wouldBlock())
+        close();
+
+    return num == len;
 }
 
-void Connection::close() {
-#ifdef WIN32
-    closesocket(sock);
-#else
-    ::close(sock);
-#endif
+void Connection::write(const std::string& data) {
+    if (!canWrite || sock == INVALID_SOCKET)
+        return;
 
-    valid = false;
+    if (::send(sock, data.c_str(), data.length(), 0) == -1) {
+        canWrite = false;
+
+        while (valid())
+            process();
+
+        close();
+    }
+}
+
+bool Connection::valid() {
+    if (!Socket::valid())
+        return false;
+
+    char discard;
+    bool valid = recv(sock, &discard, 1, MSG_PEEK) > 0 || wouldBlock();
+
+    if (!valid)
+        close();
+
+    return valid;
 }
--- GERTe/GEDS/Networking/Connection.h
+++ GERTe/GEDS/Networking/Connection.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "Socket.h"
 #include <string>
+#include <mutex>
 
 #ifdef _WIN32
 typedef unsigned long long SOCKET;
@@ -9,23 +11,55 @@ typedef int SOCKET;
 
 class DataPacket;
 
-class Connection {
-protected:
-	Connection(SOCKET, const std::string&);
-	explicit Connection(SOCKET);
+class Connection: public Socket {
+    bool canWrite = true;
+    bool outbound;
+    bool ready = false;
 
-	void close();
+    std::mutex safety;
+
+protected:
+    /**
+     * Creates a new outbound connection to a given IP address.
+     *
+     * @param addr IPv6 address
+     * @param port Remote port
+     * @param name Name to use for connection in debug log
+     */
+    Connection(uint32_t addr, uint16_t port, const std::string& name);
+
+
+    /**
+     * Creates a new outbound connection to a given IP address.
+     *
+     * @param addr IPv6 address
+     * @param port Remote port
+     * @param name Name to use for connection in debug log
+     */
+    Connection(ipv6 addr, uint16_t port, const std::string& name);
+
+    /**
+     * Wraps an existing socket with an inbound connection.
+     *
+     * @param sock Socket being wrapped
+     * @param name Name to use for connection in debug log
+     */
+    Connection(SOCKET sock, const std::string& name);
+
+    virtual void _process() = 0;
+    virtual bool handshake();
 
 public:
-	virtual ~Connection();
+    const std::string type;
+    unsigned char vers[2] = {0, 0};
 
-	SOCKET sock;
-	unsigned char vers[2] = {0, 0};
-	bool valid = false;
-
-	virtual void process() = 0;
+	void process() final;
 	virtual void send(const DataPacket&) = 0;
 
-	[[nodiscard]] std::string read(int=1) const;
-	void write(const std::string&) const;
+	[[nodiscard]] std::string read(int=1);
+	bool atleast(int);
+
+	void write(const std::string&);
+
+	bool valid() override;
 };
