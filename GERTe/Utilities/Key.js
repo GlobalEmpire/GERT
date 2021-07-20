031e3d813f31e964c8c84561944db8f96e298dda
Tyler Kuhn <contact@tyknet.tk>
2021-07-19 21:04:19 -0400

    Fixed Key.js
    Key.js now properly checks addresses before using them.
    Key.js properly responds to errors.
    Key.js puts keys into resolutions.geds as is now required.

--- GERTe/Utilities/Key.js
+++ GERTe/Utilities/Key.js
@@ -5,40 +5,100 @@ const rl = require("readline");
 const io = rl.createInterface({
 	input: process.stdin,
 	output: process.stdout,
-	prompt: "ADDRESS KEY: "
+	prompt: ""
 });
+
+const crypto = require("crypto");
+
+const addrPattern = /^(?:(\d{1,4}\.\d{1,4}):)?(\d{1,4}\.\d{1,4})$/;
+const numberPattern = /\d{1,4}/g;
 
 var fd = fs.openSync("./resolutions.geds", 'a');
 
-function byteify(str, buffer) {
+/**
+ * Tests compliance of an arbitrary string containing an address.
+ * <br>
+ * <br>
+ * Note: Modified to reject GERTc addresses
+ *
+ * @param {string} address Address to test compliance of.
+ * @returns {boolean}
+ */
+function compliant(address) {
+	const matches = address.match(addrPattern);
+	if (matches == null || matches[3])
+		return false;
+
+	for (const num of address.matchAll(numberPattern)) {
+		const numval = parseInt(num);
+		if (numval > 4095 || numval < 0)
+			return false;
+	}
+
+	return true;
+}
+
+function byteify(str) {
 	let segs = str.split(".");
 	let high = parseInt(segs[0], 10);
 	let low = parseInt(segs[1], 10);
+	let buffer = Buffer.alloc(3);
 	
 	buffer[0] = high >> 4;
 	buffer[1] = high << 4 & 0xF0 | low >> 8;
 	buffer[2] = low & 0xFF;
+	
+	return buffer;
 }
 
-function readLine(line) {
-	if (line[0] == "q") {
+function onAddress(addr) {
+	addr = addr.trim();
+
+	if (addr.toLowerCase() == "q") {
 		fs.closeSync(fd);
 		process.exit(0);
 	}
-	
-	let inputs = line.split(" ");
-	
-	let output = Buffer.alloc(23);
-	
-	let addr = byteify(inputs[0], output);
-	let key = inputs[1];
-	
-	output.write(key, 3);
-	
-	fs.writeSync(fd, output);
-	
-	io.prompt();
+
+	if (!compliant(addr)) {
+		console.error("Address is not GERTe compliant.");
+		io.question("Address: ", onAddress);
+		return;
+	}
+
+	io.question("Key path: ", (path) => {
+		path = path.trim()
+
+		if (path.toLowerCase() == "q") {
+			fs.closeSync(fd);
+			process.exit(0);
+		}
+
+		let key;
+		try {
+			key = fs.readFileSync(path);
+		} catch(e) {
+			console.error("Path not found!");
+			onAddress(addr);
+			return;
+		}
+
+		let test = crypto.createPublicKey({key: key, format: "der", type: "spki"});
+		if (test.type != "public" || test.asymmetricKeyType != "ec") {
+			console.error("Key is invalid");
+			onAddress(addr);
+			return;
+		}
+		
+		let len = Buffer.alloc(2);
+		len.writeUInt16BE(key.length);
+
+		fs.writeSync(fd, byteify(addr));
+		fs.writeSync(fd, len);
+		fs.writeSync(fd, key);
+
+		io.question("Address: ", onAddress);
+	});
 }
 
-io.on("line", readLine);
-io.prompt();
+console.log("Enter Q at any time to quit.");
+io.question("Address: ", onAddress);
