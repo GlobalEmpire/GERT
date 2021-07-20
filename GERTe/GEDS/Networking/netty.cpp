54247097ffe9d6bfc63277b1d9bb56d0d39b2bc1
Tyler Kuhn <contact@tyknet.tk>
2021-07-19 22:02:18 -0400

    Updated Netty
    Netty uses new Config class.
    Netty uses new connection and server frameworks.
    Netty no longer gets the main thread handle.
    Netty now sleeps the main thread on all systems using platform-dependent code.
    Netty uses new processor and poll system.
    Netty no longer contains any actual networking code... funny.

--- GERTe/GEDS/Networking/netty.cpp
+++ GERTe/GEDS/Networking/netty.cpp
@@ -1,178 +1,56 @@
-#ifndef _WIN32
-#include <sys/socket.h> //Load C++ standard socket API
-#include <netinet/tcp.h>
-#include <unistd.h>
-#include <poll.h>
-#include <signal.h>
-#include <netinet/ip.h>
+#ifndef WIN32
+#include <csignal>
 #else
 #define WIN32_LEAN_AND_MEAN
-#include <WinSock2.h>
-#pragma comment(lib, "Ws2_32.lib")
+#include <windows.h>
 #endif
-#include <thread>
-#include "../Peer/query.h"
-#include "../Util/logging.h"
+
 #include "../Threading/Poll.h"
-#include "../Util/Error.h"
 #include "../Threading/Processor.h"
 #include "../Gateway/DataConnection.h"
-#include "../Peer/CommandConnection.h"
-#include "../Util/Error.h"
+#include "Server.h"
+#include "../Util/logging.h"
 
 using namespace std;
 
-SOCKET gateServer, gedsServer; //Define both server sockets
+extern uint16_t gatewayPort;
+extern uint16_t peerPort;
 
-Poll socketPoll;
-Poll serverPoll;
+void runServer(const Config& config) { //Listen for new connections
+    Poll socketPoll;
+    Processor processor{ &socketPoll };
 
-#ifdef _WIN32
-bool wsastarted = false;
-#endif
-
-extern volatile bool running;
-extern unsigned short gatewayPort;
-extern unsigned short peerPort;
-extern char * LOCAL_IP;
-
-SOCKET createSocket(uint32_t ip, unsigned short port) {
-#ifdef _WIN32 //If compiled for Windows
-    if (!wsastarted) {
-        WSADATA socketConfig; //Construct WSA configuration destination
-        WSAStartup(MAKEWORD(2, 2), &socketConfig); //Initialize Winsock
-    }
-#endif
-
-    sockaddr_in tgt = {
-            AF_INET,
-            htons(gatewayPort),
-            INADDR_ANY
-    };
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    //Bind servers to addresses
-    if (connect(sock, (sockaddr*)&tgt, sizeof(sockaddr_in)) != 0) {
-        socketError("Failed connecting to peer: ");
-        return -1;
-    }
-
-    return sock;
-}
-
-void startup() {
-	//Server construction
-#ifdef _WIN32 //If compiled for Windows
-    if (!wsastarted) {
-        WSADATA socketConfig; //Construct WSA configuration destination
-        WSAStartup(MAKEWORD(2, 2), &socketConfig); //Initialize Winsock
-    }
-#endif
-
-	sockaddr_in gate = {
-			AF_INET,
-			htons(gatewayPort),
-			INADDR_ANY
-	};
-	sockaddr_in geds = {
-			AF_INET,
-			htons(peerPort),
-			INADDR_ANY
-	};
-
-	//Construct server sockets
-	gateServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //Construct gateway inbound socket
-	gedsServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //Construct GEDS P2P inbound socket
-
-	//Bind servers to addresses
-	if (::bind(gateServer, (sockaddr*)&gate, sizeof(sockaddr_in)) != 0 && errno == EADDRINUSE) { //Initialize gateway inbound socket
-		error("Gateway port is in use");
-		exit(-1);
-	}
-	if (::bind(gedsServer, (sockaddr*)&geds, sizeof(sockaddr_in)) != 0 && errno == EADDRINUSE) { //Initialize GEDS P2P inbound socket
-		error("Peer port is in use");
-		exit(-1);
-	}
-
-	//Activate servers
-	listen(gateServer, SOMAXCONN); //Open gateway inbound socket
-	listen(gedsServer, SOMAXCONN); //Open gateway inbound socket
-	//Servers constructed and started
-
-	//Add servers to poll
-	serverPoll.add(gateServer);
-	serverPoll.add(gedsServer);
-}
-
-void cleanup() {
-#ifdef _WIN32
-	closesocket(gedsServer);
-	closesocket(gateServer);
-
-	WSACleanup();
-#else
-	close(gedsServer);
-	close(gateServer);
-#endif
-
-	//Maybe later cleanup DataConnections and CommandConnections.
-}
-
-void runServer(std::vector<CommandConnection*> conns) { //Listen for new connections
-    for (auto conn: conns)
-        socketPoll.add(conn->sock, conn);
-    socketPoll.update();
-
-    std::thread serverThread { []() -> void {
-            while (running) { //Dies on SIGINT
-                Event_Data data = serverPoll.wait();
-
-                if (data.fd == 0) {
-                    return;
-                }
-
-                SOCKET newSock = accept(data.fd, nullptr, nullptr);
-
-                if (newSock == -1) {
-                    socketError("Error accepting a new connection: ");
-                    continue;
-                }
-
-#ifdef _WIN32
-                serverPoll.remove(data.fd);
-                serverPoll.add(data.fd);
-#endif
-
-                try {
-                    if (data.fd == gateServer) {
-                        auto * gate = new DataConnection(newSock);
-                        socketPoll.add(newSock, gate);
-
-#ifdef _WIN32
-                        socketPoll.update();
-#endif
-                    }
-                    else {
-                        auto peer = new CommandConnection(newSock);
-                        socketPoll.add(newSock, peer);
-
-#ifdef _WIN32
-                        socketPoll.update();
-#endif
-                    }
-                }
-                catch ([[maybe_unused]] int e) {
-#ifdef _WIN32
-                    closesocket(newSock);
-#else
-                    close(newSock);
-#endif
-                }
-            }
+    for (auto& peer: config.peers4)
+        try {
+            processor.add(new CommandConnection{peer.first, peer.second});
+        } catch(std::exception& e) {
+            error("Failed to connect to " + Config::stringify(peer.first) + ": " + e.what());
         }
-    };
 
-    serverPoll.claim(&serverThread, 1);
-    serverThread.join();
+    for (auto& peer: config.peers6)
+        try {
+            processor.add(new CommandConnection{peer.first, peer.second});
+        } catch(std::exception& e) {
+            error("Failed to connect to " + Config::stringify(peer.first) + ": " + e.what());
+        }
+
+#ifdef WIN32
+    // Alternative: Enable Dual-Stack mode. Too lazy.
+    processor.add(new Server<DataConnection>{false, gatewayPort, &processor});
+    processor.add(new Server<CommandConnection>{ false, peerPort, &processor });
+#endif
+
+    processor.add(new Server<DataConnection>{true, gatewayPort, &processor});
+    processor.add(new Server<CommandConnection>{ true, peerPort, &processor });
+
+#ifdef WIN32
+    SleepEx(INFINITE, true);
+#else
+    // Linux doesn't really have thread suspension, but we don't really need it.
+    sigset_t mask;
+    pthread_sigmask(0, nullptr, &mask);
+    sigdelset(&mask, SIGUSR1); // Unblock SIGUSR1
+
+    sigsuspend(&mask);
+#endif
 }
--- GERTe/GEDS/Networking/netty.h
+++ GERTe/GEDS/Networking/netty.h
@@ -1,22 +1,6 @@
 #pragma once
-
-#include <vector>
-#include "../Gateway/Address.h"
 #include "../Peer/CommandConnection.h"
+#include "../Files/Config.h"
+#include <vector>
 
-typedef unsigned char UCHAR;
-typedef unsigned long ULONG;
-typedef unsigned short USHORT;
-
-#ifdef _WIN32
-typedef unsigned long long SOCKET;
-#else
-typedef int SOCKET;
-#endif
-
-SOCKET createSocket(uint32_t, unsigned short);
-
-void runServer(std::vector<CommandConnection*>);
-
-void startup();
-void cleanup();
+void runServer(const Config&);
