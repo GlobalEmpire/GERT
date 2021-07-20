c31be0e60834df9bf59d46a06561b409486ec8e8
Tyler Kuhn <contact@tyknet.tk>
2021-07-19 21:10:17 -0400

    Improved Logging
    Logging is now guarded by a mutex to ensure whole lines are coherently printed.
    Logging now correctly flushes all outputs.

--- GERTe/GEDS/Util/logging.cpp
+++ GERTe/GEDS/Util/logging.cpp
@@ -2,11 +2,14 @@
 #include "logging.h"
 #include <ctime>
 #include <iostream>
+#include <mutex>
 using namespace std;
 
 extern bool debugMode;
 
 FILE * logFile;
+
+std::mutex log_lock;
 
 void startLog() {
 	logFile = fopen("errors.log", "a");
@@ -15,7 +18,7 @@ void startLog() {
 string timeOut() {
 	time_t rawtime;
 	time(&rawtime);
-	tm * curTime = localtime(&rawtime);
+	tm* curTime = localtime(&rawtime);
 
 	char time[9];
 	strftime(time, 9, "%T", curTime);
@@ -24,30 +27,43 @@ string timeOut() {
 }
 
 void log(const string& msg) {
-	cout << "[I][" << timeOut() << "] " << msg << "\n";
+    std::lock_guard guard{ log_lock };
+	cout << "[I][" << timeOut() << "] " << msg << endl;
 }
 
 void warn(const string& msg) {
-	cout << "[W][" << timeOut() << "] " << msg << "\n";
+    std::lock_guard guard{ log_lock };
+	cout << "[W][" << timeOut() << "] " << msg << endl;
 }
 
 void error(const string& msg) {
+    std::lock_guard guard{ log_lock };
 	string log = "[E][" + timeOut() + "] " + msg + "\n";
+
 	cout << log;
+	cout.flush();
+
 	fputs(log.c_str(), logFile);
 }
 
 void error2(const string& msg) {
+    std::lock_guard guard{ log_lock };
 	string log = "[E][" + timeOut() + "] " + msg;
+
 	cout << log;
+	cout.flush();
+
 	fputs(log.c_str(), logFile);
 }
 
 void debug(const string& msg) {
-	if (debugMode)
-		cout << "[D][" << timeOut() << "] " << msg << "\n";
+	if (debugMode) {
+        std::lock_guard guard{ log_lock };
+        cout << "[D][" << timeOut() << "] " << msg << endl;
+    }
 }
 
 void stopLog() {
+    std::lock_guard guard{ log_lock };
 	fclose(logFile);
 }
