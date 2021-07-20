8f511980638d361dbcf78630898a163125744eb4
Tyler Kuhn <contact@tyknet.tk>
2021-07-19 21:23:34 -0400

    Improved Entry File
    Entry file now uses better error handling functions.
    Entry file now correctly sets the thread handle for exiting the program, the responsibility is no longer on netty.
    Improved argument parsing, set size of ports explicitly, and removed unused LOCAL_IP.
    Removed -a flag and no longer require it.
    Improved Linux signal masking and handling to prevent installation-specific crashing.
    Use new Config and pass it to netty.
    Initialize new socket layer.
    Removed unneeded debug messages.

--- GERTe/GEDS/GEDS Server.cpp
+++ GERTe/GEDS/GEDS Server.cpp
@@ -11,63 +11,108 @@
  */
 #include <csignal> //Include signal processing API for error catching
 #include "Networking/netty.h" //Include netcode header for entering server process
-#include "Files/fileMngr.h" //Include file manager library for loading and saving databases
 #include "Util/Trace.h"
 #include "Util/logging.h"
+#include "Util/Error.h"
 #include <exception> //Load exception library for terminate() hook
 #include <iostream>
 #include "Util/Versioning.h"
-#include "Threading/Processor.h"
+#include "Gateway/Key.h"
+
+#ifdef WIN32
+#include <windows.h>
+#endif
+
 using namespace std; //Default namespace to std so I don't have to type out std::cout or any other crap
 
 enum status { //Create a list of error codes
 	NORMAL,
 	KEY_LOAD_ERR,
-	UNKNOWN_CRITICAL,
-	UNKNOWN_MINOR
+    UNKNOWN_MINOR,
+	UNKNOWN_CRITICAL
 };
 
 volatile bool running = false; //SIGINT tracker
 bool debugMode = false; //Set debug mode to false by default
 
-char * LOCAL_IP = nullptr; //Set local address to predictable null value for testing
-unsigned short peerPort = 59474; //Set default peer port
-unsigned short gatewayPort = 43780; //Set default gateway port
+uint16_t peerPort = 59474; //Set default peer port
+uint16_t gatewayPort = 43780; //Set default gateway port
 
-extern Poll serverPoll;
-extern Poll socketPoll;
+#ifdef WIN32
+HANDLE sthread;
 
-void shutdownProceedure([[maybe_unused]] int param) { //SIGINT handler function
+extern void apc(ULONG_PTR);
+#endif
+
+void shutdownProceedure([[maybe_unused]] int _) { //SIGINT handler function
 	if (running) { //If we actually started running
 		warn("User requested shutdown. Flipping the switch!"); //Notify the user because reasons
 		running = false; //Flip tracker to disable threads and trigger main thread's cleanup
 
 #ifdef _WIN32
-		serverPoll.update(); //Work around Windows oddities
+        QueueUserAPC(apc, sthread, 0);
 #endif
 	} else { //We weren't actually running?
 		error("Server wasn't in running state when SIGINT was raised."); //Warn user of potential error
 		error("!!! Forcing termination of server. !!!"); //Warn user we are stopping
+        cout << "\nAbnormal Exit\n";
 		exit(UNKNOWN_MINOR); //Exit with correct exit code
 	}
 }
 
-void OHCRAPOHCRAP([[maybe_unused]] int param) { //Uhm, we've caused a CPU error
+void OHCRAPOHCRAP([[maybe_unused]] int _) { //Uhm, we've caused a CPU error
 	running = false;
 	dumpStack();
 	cout << "\nSIGSEGV THROWN: SEGMENTATION FAULT.\n";
 	cout << "Please post a bug report including any potentially useful logs.\n";
+    cout << "\nAbnormal Exit\n";
 	exit(UNKNOWN_CRITICAL); //Exit with correct exit code
 }
 
 void errHandler() { //Error catcher, provides minor error recovery facilities
 	running = false;
 	cout << "Unknown error, system called terminate() with code " << to_string(errno) << "\n"; //Report fault to user
+	cout << "Possibly " << std::make_error_code((std::errc)errno).message() << "\n";
+
+	cout << "Testing for C++ exception.\n";
+
+	const int exceptions = uncaught_exceptions();
+	cout << "Uncaught exceptions: " << to_string(exceptions) << "\n";
+
+	for (int i = 0; i < exceptions; i++) {
+	    auto ptr = current_exception();
+
+	    if (ptr) {
+	        try {
+                rethrow_exception(ptr);
+	        } catch (std::exception& e) {
+                cout << "Exception " << to_string(i) << ": " << e.what() << "\n";
+	        }
+	    }
+	}
+
+	if (exceptions == 0) {
+        auto ptr = current_exception();
+
+        if (ptr) {
+            cout << "No exceptions but we found one?\n";
+
+            try {
+                rethrow_exception(ptr);
+            } catch (std::exception& e) {
+                cout << "Exception: " << e.what() << "\n";
+            }
+        }
+	}
+
+
+	cout << "\nAbnormal Exit\n";
 	exit(UNKNOWN_CRITICAL); //Exit with correct exit code
 }
 
+void shandler([[maybe_unused]] int _) {}
+
 inline void printHelp() { //Prints help on parameters
-	cout << "Requires atleast the -a parameter\n";
 	cout << "-a publicIP   Specifies the public IP to prevent loops.\n";
 	cout << "-p port       Specifies the port for GEDS servers to connect to (default 59474)\n";
 	cout << "-g port       Specifies the port for Gateways to connect to (default 43780)\n";
@@ -81,33 +126,37 @@ void processArgs(int argc, char* argv[]) { //Process and interpret command line 
 		string curArg = argv[i]; //Convert it to a string for easier access
 		if (curArg == "-d") { //Process the -d Debug messages flag
 			debugMode = true;
-		} else if (curArg == "-a") { //Process the local address flag (REQUIRED)
-			if (argc == i) //Address flag is missing the actual address
-				printHelp(); //Print help, user might not know how to use the flag
-			LOCAL_IP = argv[++i]; //Local address set to next argument. Also skips the next argument
 		} else if (curArg  == "-p") { //Process the local GEDS port flag
 			if (argc == i) //If the port flag is missing the actual port
 				printHelp(); //Print help, user might not know how to use the flag
-			peerPort = atoi(argv[++i])  ; //Local GEDS port is set to the next argument. Also skips the next argument
+
+            std::string nextArg = argv[++i];
+			peerPort = std::stoul(nextArg); //Local GEDS port is set to the next argument. Also skips the next argument
 		} else if (curArg == "-g") { //Process the gateway port flag
 			if (argc == i) //If the port flag is missing the actual port
 				printHelp(); //Print help, user might not know how to use the flag
-			gatewayPort = atoi(argv[++i]); //Local GEDS port is set to the next argument. Also skips the next argument
+
+            std::string nextArg = argv[++i];
+			gatewayPort = std::stoul(nextArg); //Local GEDS port is set to the next argument. Also skips the next argument
 		} else if (curArg == "-h") //Process the help flag
 				printHelp(); //Prints help, user requested it
 	}
-	if (LOCAL_IP == nullptr) //If the local address was not set
-		printHelp(); //User forgot to include local address, print help in case they don't know they have to
 }
 
 int main( int argc, char* argv[] ) {
 	cout << "GEDS Server v" << ThisVersion.stringify() << endl; //Print version information
-	cout << "Copyright 2017" << endl; //Print simple copyright information
+	cout << "Copyright 2021" << endl; //Print simple copyright information
+
+    startLog(); //Create log handles
 
 	processArgs(argc, argv); //Process command line argument
-	debug((string)"Processed arguments. Gateway port: " + to_string(gatewayPort) + " Peer port: " + to_string(peerPort) + " Local IP: " + LOCAL_IP + " and debug output"); //Print to debug processed arguments
+	debug((string)"Processed arguments. Gateway port: " + to_string(gatewayPort) + " Peer port: " +
+	    to_string(peerPort) + " and debug output"); //Print to debug processed arguments
 
-	startLog(); //Create log handles
+#ifdef WIN32
+    debug("Hello, this is an unreleased build. It has a few issues. Most notably, on some machines, decryption errors can cause crashing.");
+    debug("If you encounter this bug, PLEASE contact me, I need the help. I have no idea what causes this. It should literally be impossible.");
+#endif
 
 	set_terminate(errHandler);
 
@@ -115,41 +164,58 @@ int main( int argc, char* argv[] ) {
 	signal(SIGSEGV, &OHCRAPOHCRAP); //Catches the SIGSEGV CPU fault
 	signal(SIGINT, &shutdownProceedure); //Hook SIGINT with custom handler
 #else
-	struct sigaction mem = { 0 };
-	struct sigaction intr = { 0 };
-
-	mem.sa_handler = OHCRAPOHCRAP;
-	intr.sa_handler = shutdownProceedure;
+	struct sigaction mem = { &OHCRAPOHCRAP };
+	struct sigaction intr = { &shutdownProceedure };
+	struct sigaction usr = { &shandler };
 
 	sigaction(SIGSEGV, &mem, nullptr);
 	sigaction(SIGINT, &intr, nullptr);
+	sigaction(SIGUSR1, &usr, nullptr);
+
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1); // Block SIGUSR1 to prevent racing
+    pthread_sigmask(SIG_BLOCK, &mask, nullptr);
 #endif
+
 	debug("Loading key registrations");
-	if (!loadResolutions())
+	if (!Key::loadKeys("resolutions.geds"))
 	    return KEY_LOAD_ERR;
 
-	debug("Starting servers"); //Use debug to notify user where we are in the loading process
-	startup(); //Startup server sockets
+	debug("Loading configuration");
+	Config config{ "geds.conf" };
+
+	Socket::init();
 
 	debug("Building peer web"); //Use debug to notify user where we are in the loading process
-	auto peers = loadPeers();
+
+#ifdef WIN32
+    {
+        auto proc = GetCurrentProcess();
+        DuplicateHandle(proc, GetCurrentThread(), proc, &sthread, 0, false, DUPLICATE_SAME_ACCESS);
+    }
+#endif
 
 	running = true; //We've officially started running! SIGINT is now not evil!
 
-	debug("Starting transport processor"); //Use debug to notify user where we are in the loading process
-    { // New block to abuse built-in C++ memory management.
-        Processor gateways{ &socketPoll };
+    debug("Starting main server loop"); //Use debug to notify user where we are in the loading process
 
-        debug("Starting main server loop"); //Use debug to notify user where we are in the loading process
-        runServer(peers); //Process incoming connections (not messages)
-        warn("Primary server killed."); //Notify user we've stopped accepting incoming connections
-
-        //Shutdown and Cleanup sequence
-        debug("Cleaning up servers"); //Notify user where we are in the shutdown process
-        cleanup(); //Cleanup servers
-        debug("Waiting for transport processor to exit"); //Notify user where we are in the shutdown process
+    try {
+        runServer(config); //Process incoming connections (not messages)
+    } catch (std::exception& e) {
+        error("Uncaught exception");
+        throw e;
     }
-	debug("Transport processor exited");
+
+    warn("Main server loop killed."); //Notify user we've stopped accepting incoming connections
+
+    running = false;
+
+#ifdef WIN32
+    CloseHandle(sthread);
+#endif
+
+    Socket::deinit();
 
 	stopLog(); //Release log handles
 
