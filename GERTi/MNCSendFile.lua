f8e2bcfad4925bede6146fc78b6166e241c74826
leothehero <leothehero@users.noreply.github.com>
2021-11-18 18:24:07 +0100

    Update MNCSendFile.lua

--- GERTi/MNCSendFile.lua
+++ GERTi/MNCSendFile.lua
@@ -1,3 +1,17 @@
-require("GERTiClient")
+local GERTi = require("GERTiClient")
+local fs = require("filesystem")
+
+local ModuleFolder = ""
+local IDFilePath = "/moduleListeners"
 
 local SendFile = function (address,filename)
+
+end
+
+
+
+
+
+if fs.exists(IDFilePath) and fs.isDirectory(IDFilePath) then
+    file 
+event.register
