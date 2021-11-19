66588ffe0c3ff6724a50466733cbe5baf8f2d9ab
leothehero <leothehero@users.noreply.github.com>
2021-11-19 23:49:10 +0100

    Starting work on Client

---
+++ GERTi/Modules/GERTCUpdater.lua
@@ -0,0 +1,8 @@
+local GERTi = require("GERTiClient")
+local fs = require("filesystem")
+local internet = require("internet")
+local event = require("event")
+local shell = require("shell")
+
+local updatePort = 941
+local ModuleFolder = "/usr/lib/"
