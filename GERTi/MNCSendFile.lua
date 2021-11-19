a41ee617c332e2f298654bbb788e01d3b902dc3a
leothehero <leothehero@users.noreply.github.com>
2021-11-19 21:24:41 +0100

    Update MNCSendFile.lua

--- GERTi/MNCSendFile.lua
+++ GERTi/MNCSendFile.lua
@@ -16,17 +16,51 @@ end
 local function SearchForProgram(data)
     local completeRemoteURL = mainRemoteDirectory .. fs.name(data[2]) .. "/latest/" .. fs.name(data[3])
     local remoteFile = internet.request(completeRemoteURL)
-    local tempFileDownload = remoteFile:read(100)
-    if string.find("404: Not Found") then
+    local succeed,tempFileDownload = pcall(function()
+        local tempFileDownload = ""
+        for fileChunk in remoteFile do
+            tempFileDownload = tempFileDownload .. fileChunk
+        end
+        return tempFileDownload
+    end)
+    if not succeed then
         --Here would be where we initiate the check for a (mainRemoteDirectory .. ".removed") check
         remoteFile:close()
         return false, 1 -- 1 means file not found on remote
     end
-    local CacheFile = io.open(moduleCachePath .. fs.name(data[2]) .. "/" .. fs.name(data[3]))
-    CacheFile:write()
+    local storageDrive = fs.get(moduleCachePath)
+    local completeModuleCachePath = moduleCachePath .. fs.name(data[2]) .. "/" .. fs.name(data[3])
+    local remainingSpace = fs.size(completeModuleCachePath) + (storageDrive.spaceTotal()-storageDrive.spaceUsed())
+    fileLen = string.len(tempFileDownload)
+    if fileLen < remainingSpace-200 then
+        local CacheFile = io.open(completeModuleCachePath,"w")
+        CacheFile:write(tempFileDownload)
+        CacheFile:close()
+        return true, completeModuleCachePath
+    else
+        return false, 2 -- 2 means Insufficient Space To Download File On MNC. Contact Admin if returned
+    end
 end
 
+local function SearchForCache(data)
+    local completeModuleCachePath = moduleCachePath .. fs.name(data[2]) .. "/" .. fs.name(data[3])
+    if fs.exists(completeModuleCachePath) then
+        return completeModuleCachePath
+    else
+        return false
+    end
+end
 
+local function SendCachedFile(originAddress,data)
+    local completeModuleCachePath = moduleCachePath .. fs.name(data[2]) .. "/" .. fs.name(data[3])
+    local fileToSend = io.open(completeModuleCachePath)
+    local chunk = fileToSend:read(8000)
+    updateSockets[originAddress]:write(chunk)
+    while string.len(chunk) == 8000 do
+            
+    end
+
+end
 
 
 
@@ -40,9 +74,24 @@ end
 local function HandleData(_,originAddress,connectionID,data)
     if connectionID == updatePort and updateSockets[originAddress] then
         data = updateSockets[originAddress]:read()
-        if type(data[1]) == "table" and data[1] == "ModuleUpdate" then
-            updateSockets[originAddress]:write("U.RequestReceived",data[3])
-            SearchForProgram(data[1])
+        if type(data[1]) == "table" then 
+            if data[1] == "ModuleUpdate" then
+                updateSockets[originAddress]:write("U.RequestReceived",data[3])
+                local success, information = SearchForProgram(data[1])
+                os.sleep(0.1)
+                if not(success) then
+                    if information == 2 then
+                        updateSockets[originAddress]:write(false,2)
+                    elseif information == 1 then
+                        information = searchForCache(data[1])
+                        if information then
+                            updateSockets[originAddress]:write(true,fs.size(information))
+                        end
+                    end
+                end
+            elseif data[1] == "RequestCache" then
+                SendCachedFile(originAddress,data)
+            end
         end
     end
 end
