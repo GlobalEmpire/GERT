024073436855c1c1b1984b28fd7e6f5076f54067
leothehero <leothehero@users.noreply.github.com>
2021-11-20 00:45:46 +0100

    Update GERTCUpdater.lua

--- GERTi/Modules/GERTCUpdater.lua
+++ GERTi/Modules/GERTCUpdater.lua
@@ -25,6 +25,7 @@ GERTUpdaterAPI.GetLocalVersion = function(path)
 end
 
 GERTUpdaterAPI.GetRemoteVersion = function(moduleName,socket)
+    local size, state, version = 0, 0, ""
     local hadSocket = true
     if not socket then
         hadSocket = false
@@ -35,7 +36,38 @@ GERTUpdaterAPI.GetRemoteVersion = function(moduleName,socket)
         end
     end
     socket:write("ModuleUpdate",moduleName)
-
+    local response = event.pull(10, "GERTData", updateAddress, updatePort)
+    if not response then
+        return false, 2 -- 2 means timeout
+    end
+    local data = socket:read()
+    if type(data[1]) == "table" then 
+        if data[1][1] == "U.RequestReceived" then
+            response = event.pull(10, "GERTData", updateAddress, updatePort)
+            if not response then
+                return false, 2 -- 2 means timeout
+            else
+                data = socket:read()
+                if type(data[1]) == "table" and  then
+                    if data[1][1] == true then
+                        size, state, version = data[1][2],data[1][3],data[1][4]
+                        return true, size, state, version
+                    else
+                else
+                    return false, 3, data[1] -- 3 means unknown error, passing back unexpected output
+                end
+            end
+        else
+            socket:close()
+            return false, 3, data[1][1] -- 3 means unknown error, passing back unexpected output
+        end
+    else
+        socket:close()
+        return false, 3, data[1] -- 3 means unknown error, passing back unexpected output
+    end
+    if not hadSocket then
+        socket:close()
+    end
 end
 
 
@@ -54,8 +86,8 @@ GERTUpdaterAPI.CheckForUpdate = function (moduleName)
         for trueModuleName, modulePath in moduleName do
             local localVersion = GERTUpdaterAPI.GetLocalVersion(modulePath)
             local localSize = fs.size(modulePath)
-            local remoteVersion, remoteSize = GERTUpdaterAPI.GetRemoteVersion
-            if connectionComplete then
+            local success, remoteSize, statusCode, remoteVersion = GERTUpdaterAPI.GetRemoteVersion()
+            if success then
                 
             else
                 remoteVersion,remoteSize
