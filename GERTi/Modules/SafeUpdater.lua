03ca829ad6d381bd3189f4e4f1bb1734d41cb9fc
leothehero <leothehero@users.noreply.github.com>
2021-11-20 19:59:02 +0100

    Update SafeUpdater.lua

--- GERTi/Modules/SafeUpdater.lua
+++ GERTi/Modules/SafeUpdater.lua
@@ -1,25 +1,31 @@
 local event = require("event")
 local fs = require("filesystem")
 local srl = require("serialization")
+local term = require("term")
 
 local UpdaterAPI = {}
 
+local function checkTerm (text)
+    if term.isAvailable() then
+        term.write(text,
+    end
+end
+
 local function ParseSafeList ()
     if not fs.exists("/.SafeUpdateCache.srl") then 
-        return {}
+        return false
     end
     local file = io.open("/.SafeUpdateCache.srl", "r")
     local rawData = file:read("*a")
     file:close()
     local parsedData = srl.unserialize(rawData)
-    return parsedData or {}
+    return parsedData or false
 end
 
-local function RemoveFromSafeList (moduleName)
+local function RemoveFromSafeList (moduleName,parsedData)
     if not moduleName then
         return false
     end
-    local parsedData = ParseSafeList()
     fs.remove(parsedData[moduleName][2])
     parsedData[moduleName] = nil
     local file = io.open("/.SafeUpdateCache.srl", "w")
@@ -39,9 +45,19 @@ local function InstallUpdate (moduleName,parsedData)
 end
 
 UpdaterAPI.start = function()
+    io.stdout:write("Parsing GERTUpdate SafeFile\n")
     local parsedData = ParseSafeList()
+    if not parsedData then
+        io.stdout:write("No Updates Queued\n")
+        os.sleep(0.5)
+        return
+    end
+    io.stdout:write("Updates Found\n")
+    os.sleep(0.3)
+    io.stderr:write("INITIATING PRE-SHELL SECURE UPDATE PROGRAM")
+    gpu.getResolution
     for name, Information in pairs(parsedData) do
-        RemoveFromSafeList(InstallUpdate(name,parsedData))
+        RemoveFromSafeList(InstallUpdate(name,parsedData),parsedData)
     end
 end
 
