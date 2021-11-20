862f30c206bb9d062fdd09c76b90ea10d4d3d263
leothehero <leothehero@users.noreply.github.com>
2021-11-20 14:07:48 +0100

    Update SafeUpdater.lua

--- GERTi/Modules/SafeUpdater.lua
+++ GERTi/Modules/SafeUpdater.lua
@@ -1,19 +1,47 @@
 local event = require("event")
 local fs = require("filesystem")
-local shell = require("shell")
 local srl = require("serialization")
 
-local args, opts = shell.parse(...)
+local UpdaterAPI = {}
 
-local GERTUpdaterAPI = {}
+local function ParseSafeList ()
+    if not fs.exists("/.SafeUpdateCache.srl") then 
+        return {}
+    end
+    local file = io.open("/.SafeUpdateCache.srl", "r")
+    local rawData = file:read("*a")
+    file:close()
+    local parsedData = srl.unserialize(rawData)
+    return parsedData or {}
+end
 
+local function RemoveFromSafeList (moduleName)
+    if not moduleName then
+        return false
+    end
+    local parsedData = ParseSafeList()
+    fs.remove(parsedData[moduleName][2])
+    parsedData[moduleName] = nil
+    local file = io.open("/.SafeUpdateCache.srl", "w")
+    file:write(srl.serialize(parsedData))
+    file:close()
+    return true
+end
 
+local function InstallUpdate (moduleName,parsedData)
+    local success = filesystem.copy(parsedData[moduleName][2],parsedData[moduleName][1])
+    if success then
+        return moduleName
+    else
+        parsedData[moduleName][3] = 2
+        return false
+    end
+end
 
+UpdaterAPI.start = function()
+    local parsedData = ParseSafeList()
+    for name, Information in pairs(parsedData) do
+        RemoveFromSafeList(InstallUpdate(name,parsedData))
+    end
+end
 
-
-
-
-
-event.listen("InstallReady",InstallEventHandler)
-
-return GERTUpdaterAPI
