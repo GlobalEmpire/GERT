1687d4c98b9100b74463161b02ca30f675437dae
MajorGeneralRelativity <majgenrelativity@gmail.com>
2021-11-24 15:59:36 -0500

    Update MNCInstaller.lua

--- GERTi/MNCInstaller.lua
+++ GERTi/MNCInstaller.lua
@@ -1,3 +1,4 @@
 local shell = require("shell")
 shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiMNC.lua /etc/rc.d/GERTiMNC.lua")
-shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/MNCAPI.lua /lib/MNCAPI.lua")
+shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/MNCAPI.lua /lib/GERTiClient.lua")
+shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/Modules/DNS.lua /etc/rc.d/DNS.lua")
