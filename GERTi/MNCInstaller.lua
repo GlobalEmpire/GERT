3324eb2f21e57ea93b1d51ada41becf4b463dcdc
leothehero <leothehero@users.noreply.github.com>
2021-11-30 01:31:50 +0100

    Improved MNCInstaller.lua (#103)
    * Update GERTiMNC.lua
    
    Adding a small script that checks for the presence of an auto updater program, and writes the shutdown listener's event id to a file if so.
    
    * Added Update Source
    
    Added some files to allow updaters to properly locate their source file
    
    * Revert "Update GERTiMNC.lua"
    
    This reverts commit f5443ee211d8c0ebb9efeefbb56cd60ba5d634f7.
    
    * Update GERTiClient Stable Release.txt
    
    * Update GERTiClient Stable Release.txt
    
    * Update GERTiClient Stable Release.txt
    
    * Update GERTiMNC Stable Release.txt
    
    * Update GERTiMNC Stable Release.txt
    
    * Update GERTiMNC Stable Release.txt
    
    * Update MNCInstaller.lua
    
    * Update MNCInstaller.lua
    
    * Update GERTCUpdater.lua
    
    * Fixed variable index crash bug
    
    * Fixed ipairs() instead of pairs()
    
    * Release 1.1:
    
    Fixed the logic that actually sends files
    
    * version test
    
    * Revert "version test"
    
    This reverts commit dda987679a7f89025666b5071434c38bf1c975f1.
    
    * Update MNCInstaller.lua
    
    * Update MNCInstaller.lua

--- GERTi/MNCInstaller.lua
+++ GERTi/MNCInstaller.lua
@@ -1,11 +1,27 @@
 local shell = require("shell")
 shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/GERTiMNC.lua /etc/rc.d/GERTiMNC.lua -f")
 shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/Modules/MNCAPI.lua /lib/GERTiClient.lua -f")
+local fs = require("filesystem")
+if not fs.isDirectory("/usr/lib") then
+    fs.makeDirectory("/usr/lib")
+end
 shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/Modules/GERTUpdateServer.lua /usr/lib/GERTUpdateServer.lua -f")
 shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/Modules/DNS.lua /etc/rc.d/DNS.lua -f")
 shell.execute("wget https://raw.githubusercontent.com/GlobalEmpire/GERT/master/GERTi/Modules/RecommendedConfig-GUSs.cfg /etc/GERTUpdateServer.cfg")
-local tempfile = io.open("/.autorun","a")
-tempfile:write("\nrequire('GERTUpdateServer')")
+local autorunList = {"/autorun","/autorun.lua","/.autorun","/.autorun.lua"}
+local autorunDirectory = "/autorun"
+for k,v in ipairs(autorunList) do
+    if fs.exists(v) then
+        autorunDirectory = v
+    end
+end
+local tempfile = io.open(autorunDirectory,"r")
+local fullAutoRun = tempfile:read("*a")
 tempfile:close()
+if not string.find(fullAutoRun,"require('GERTUpdateServer')") then
+    local tempfile = io.open("/.autorun","a")
+    tempfile:write("\nrequire('GERTUpdateServer')")
+    tempfile:close()
+end
 shell.execute("rc GERTiMNC enable")
 shell.execute("rc DNS enable")
