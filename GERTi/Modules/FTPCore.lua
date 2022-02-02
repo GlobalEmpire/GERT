8c3709dedd0e906409605a1fb053de9e30395210
leothehero <leothehero@users.noreply.github.com>
2022-02-02 20:10:27 +0100

    Update FTPCore.lua

--- GERTi/Modules/FTPCore.lua
+++ GERTi/Modules/FTPCore.lua
@@ -16,6 +16,7 @@ local NOREMOTEFILE = -8
 local CANNOTCHANGE = -9
 local UPTODATE = -10
 local NOREMOTERESPONSE = -11
+local STUCK = -12
 
 --Op Codes
 local ALLGOOD = 0
@@ -82,11 +83,11 @@ FTPInternal.DownloadFile = function (FileDetails,FileData,socket) --Provide file
                 destfile:write(socket:read()[2])
             end
             loopDone = INTERRUPTED
-        elseif #socket:read("-k")[1] == "FTPDATASENT" then
+        elseif socket:read("-k")[1] == "FTPDATASENT" then
             destfile:write(socket:read()[2])
             socket:write("FTPREADYTOCONTINUERECEIVE")
-        elseif #socket:read("-k")[1] == "FTPDATAFIN" then
-            destfile:write(socket:read()[2])
+        elseif socket:read("-k")[1] == "FTPDATAFIN" then
+            loopDone = ALLGOOD
         end
     until loopDone
     destfile:close()
@@ -98,28 +99,48 @@ FTPInternal.DownloadFile = function (FileDetails,FileData,socket) --Provide file
 end
 
 
-FTPInternal.SendFile = function (file,directory,address,port,user,auth) -- returns true if successful, false if timeout or invalid modulename
+FTPInternal.SendFile = function (FileDetails,StepComplete,socket) -- returns true if successful, false if timeout or invalid modulename
     --Do Auth Processing in a separate function and append it to destination. if not user then user = "public"
-    local fileLocation = directory .. "/" .. file
-
-    local fileToSend = io.open(fileLocation, "rb")
-
+    if not StepComplete then
+        return false, socket
+    end
+    local fileToSend = io.open(FileDetails.file, "rb")
     local chunk = fileToSend:read(8000)
+    local sendState = "FTPDATASENT"
+    local lastState = ALLGOOD
+    local loopStuck = 0
     while chunk ~= nil and chunk ~= "" do
-        updateSockets[originAddress]:write(chunk)
-        local success = event.pull(10, "GERTData", originAddress, updatePort)
+        socket:write(sendState,chunk)
+        local success = event.pullFiltered(5, function (eventName, iAdd, dAdd, CID) if (iAdd == FileDetails.address or dAdd == FileDetails.address) and (dAdd == FileDetails.port or CID == FileDetails.port) then if eventName == "GERTConnectionClose" or eventName == "GERTData" then return true end end return false end)
+        if success == "GERTConnectionClose" then
+            fileToSend:close()
+            return false, INTERRUPTED
+        end
+        while success and #socket:read("-k") == 0 do
+            os.sleep() -- \\\\This might need a lengthening to 0.1, if OC is weird.\\\\
+        end
+        local returnData = socket:read()
         if not success then
-            updateSockets[originAddress]:close()
             fileToSend:close()
             return false, TIMEOUT
+        elseif returnData == "FTPREADYTOCONTINUERECEIVE" then
+            loopStuck = 0
+            sendState = "FTPDATASENT"
+            chunk = fileToSend:read(8000)
+        elseif returnData == "FTPREPEATDATA" and loopStuck < 5 then --FutureProofing
+            sendState = "FTPREPEATINGSEND"
+            loopStuck = loopStuck + 1
+        elseif returnData == "FTPCANCEL" then
+            chunk = nil
+            lastState = UNKNOWN
+        elseif loopStuck == 5 then
+            lastState = STUCK
+            chunk = nil
         end
-        chunk = fileToSend:read(8000)
     end
+    socket:write("FTPDATAFIN")
     fileToSend:close()
-    os.sleep(0.5)
-    updateSockets[originAddress]:close()
-    updateSockets[originAddress] = nil
-    return true, ALLGOOD
+    return true, lastState
 end
 
 FTPCore.DownloadFile = function (FileDetails)
@@ -131,7 +152,15 @@ FTPCore.DownloadFile = function (FileDetails)
     local StepComplete,socket = FTPInternal.CreateValidSocket(FileDetails)
     local FileData,result = FTPInternal.CheckData(FileDetails,StepComplete,socket)
     local StepComplete,result = FTPInternal.DownloadFile(FileDetails,FileData,result or socket)
+    if socket.close then
+        socket:close()
+    end
+    return StepComplete,result
 end
 
+FTPCore.SendFile = function (FileDetails)
+    local StepComplete,socket = FTPInternal.CreateValidSocket(FileDetails)
+    local StepComplete,result = FTPInternal.SendFile(FileDetails, StepComplete, socket)
+end
 
 return FTPCore
